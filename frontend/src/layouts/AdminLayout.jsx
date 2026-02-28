23d8566579f18c1ac30d78965ff23c046cd8e31b
Arpittiwari1 <arpittiwari0408+1@gmail.com>
2026-02-28 21:44:19 +0530

    Initial commit with frontend and backend

---
+++ frontend/src/layouts/AdminLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+const AdminLayout = () => {
+  return (
+    <div>
+      <aside>Admin Sidebar</aside>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AdminLayout;
